311ae255ba00def4088679f550fcfe3f065487e2
Clikuki <lanzvasquez24@gmail.com>
2024-06-30 21:28:29 +0800

    added piece type object

---
+++ src/main/kotlin/Piece.kt
@@ -0,0 +1,11 @@
+object Piece {
+    const val NONE: Byte = 0b00_000
+    const val PAWN: Byte = 0b00_001
+    const val BISHOP: Byte = 0b00_010
+    const val KNIGHT: Byte = 0b00_011
+    const val ROOK: Byte = 0b00_100
+    const val QUEEN: Byte = 0b00_101
+    const val KING: Byte = 0b00_110
+    const val WHITE: Byte = 0b01_000
+    const val BLACK: Byte = 0b10_000
+}
