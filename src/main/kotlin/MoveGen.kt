66b0d8979cba1a9e075aad51c93a2327c383c12b
Clikuki <lanzvasquez24@gmail.com>
2024-07-10 10:33:09 +0800

    moved move generation data into dedicated moveGenData

--- src/main/kotlin/MoveGen.kt
+++ src/main/kotlin/MoveGen.kt
@@ -3,20 +3,18 @@ import kotlin.experimental.and
 fun lsb(bb: ULong) = bb.takeLowestOneBit().let { it to it.countTrailingZeroBits() }
 
 private fun generateOrthogonalMoves(
-    board: Board,
     moves: MutableList<Move>,
+    data: MoveGenData,
 ) {
-    var sliders = board.getQueenBB() or board.getRookBB()
+    var sliders = data.board.getQueenBB() or data.board.getRookBB()
     if (sliders == 0UL) return
 
-    val enemy = board.getOpponentBB()
-    val friendly = board.getColorBB()
     var (lsb1, from) = lsb(sliders)
     while (lsb1 != 0UL) {
-        val north = Occl.nort(lsb1, (friendly or Shift.nort(enemy, 1)).inv())
-        val south = Occl.sout(lsb1, (friendly or Shift.sout(enemy, 1)).inv())
-        val west = Occl.west(lsb1, (friendly or Shift.west(enemy)).inv())
-        val east = Occl.east(lsb1, (friendly or Shift.east(enemy)).inv())
+        val north = Occl.nort(lsb1, (data.ownPieces or Shift.nort(data.oppPieces, 1)).inv())
+        val south = Occl.sout(lsb1, (data.ownPieces or Shift.sout(data.oppPieces, 1)).inv())
+        val west = Occl.west(lsb1, (data.ownPieces or Shift.west(data.oppPieces)).inv())
+        val east = Occl.east(lsb1, (data.ownPieces or Shift.east(data.oppPieces)).inv())
         var rays = (north or south or west or east) xor lsb1
 
         var (lsb2, to) = lsb(rays)
@@ -39,20 +37,18 @@ private fun generateOrthogonalMoves(
 }
 
 private fun generateDiagonalMoves(
-    board: Board,
     moves: MutableList<Move>,
+    data: MoveGenData,
 ) {
-    var sliders = board.getQueenBB() or board.getBishopBB()
+    var sliders = data.board.getQueenBB() or data.board.getBishopBB()
     if (sliders == 0UL) return
 
-    val enemy = board.getOpponentBB()
-    val friendly = board.getColorBB()
     var (lsb1, from) = lsb(sliders)
     while (lsb1 != 0UL) {
-        val noWe = Occl.noWe(lsb1, (friendly or Shift.noWe(enemy)).inv())
-        val noEa = Occl.noEa(lsb1, (friendly or Shift.noEa(enemy)).inv())
-        val soWe = Occl.soWe(lsb1, (friendly or Shift.soWe(enemy)).inv())
-        val soEa = Occl.soEa(lsb1, (friendly or Shift.soEa(enemy)).inv())
+        val noWe = Occl.noWe(lsb1, (data.ownPieces or Shift.noWe(data.oppPieces)).inv())
+        val noEa = Occl.noEa(lsb1, (data.ownPieces or Shift.noEa(data.oppPieces)).inv())
+        val soWe = Occl.soWe(lsb1, (data.ownPieces or Shift.soWe(data.oppPieces)).inv())
+        val soEa = Occl.soEa(lsb1, (data.ownPieces or Shift.soEa(data.oppPieces)).inv())
         var rays = (noWe or noEa or soWe or soEa) xor lsb1
         var (lsb2, to) = lsb(rays)
         while (rays != 0UL) {
@@ -86,31 +82,27 @@ private fun generatePromotions(
 }
 
 private fun generatePawnMoves(
-    board: Board,
     moves: MutableList<Move>,
+    data: MoveGenData,
 ) {
-    val isWhite = board.side == Piece.WHITE
-    val forwardOffset = if (isWhite) -8 else 8
-    val shifter = if (isWhite) Shift::nort else Shift::sout
+    val forwardOffset = if (data.isWhite) -8 else 8
+    val shifter = if (data.isWhite) Shift::nort else Shift::sout
 
-    val promotionRank = if (isWhite) RANK_8 else RANK_1
+    val promotionRank = if (data.isWhite) RANK_8 else RANK_1
     val nonPromoRanks = promotionRank.inv()
-    val dblPushRank = if (isWhite) RANK_2 else RANK_7
+    val dblPushRank = if (data.isWhite) RANK_2 else RANK_7
 
-    val pawns = board.getPawnBB()
-    val capturable = board.getOpponentBB()
-    val emptySqrs = board.occupancyBB.inv()
+    val pawns = data.board.getPawnBB()
+    var normalPawns = shifter(pawns and nonPromoRanks, 1) and data.emptySqrs
+    var promoPawns = shifter(pawns and promotionRank, 1) and data.emptySqrs
+    var dblPushPawns = shifter(pawns and dblPushRank, 2) and (data.emptySqrs and shifter(data.emptySqrs, 1))
 
-    var normalPawns = shifter(pawns and nonPromoRanks, 1) and emptySqrs
-    var promoPawns = shifter(pawns and promotionRank, 1) and emptySqrs
-    var dblPushPawns = shifter(pawns and dblPushRank, 2) and (emptySqrs and shifter(emptySqrs, 1))
-
-    val left = if (isWhite) Shift::noWe else Shift::soWe
-    val right = if (isWhite) Shift::noEa else Shift::soEa
-    var normalCaptureLeft = left(pawns and nonPromoRanks) and capturable
-    var normalCaptureRight = right(pawns and nonPromoRanks) and capturable
-    var promoCaptureLeft = left(pawns and promotionRank) and capturable
-    var promoCaptureRight = right(pawns and promotionRank) and capturable
+    val left = if (data.isWhite) Shift::noWe else Shift::soWe
+    val right = if (data.isWhite) Shift::noEa else Shift::soEa
+    var normalCaptureLeft = left(pawns and nonPromoRanks) and data.oppPieces
+    var normalCaptureRight = right(pawns and nonPromoRanks) and data.oppPieces
+    var promoCaptureLeft = left(pawns and promotionRank) and data.oppPieces
+    var promoCaptureRight = right(pawns and promotionRank) and data.oppPieces
 
 //    Normal push
     if (normalPawns != 0UL) {
@@ -227,10 +219,10 @@ private fun generatePawnMoves(
         }
     }
 
-    if (board.enpassantTarget != -1) {
-        val leftPawn = board.enpassantTarget - 1
-        val rightPawn = board.enpassantTarget + 1
-        val to = board.enpassantTarget + forwardOffset
+    if (data.board.enpassantTarget != -1) {
+        val leftPawn = data.board.enpassantTarget - 1
+        val rightPawn = data.board.enpassantTarget + 1
+        val to = data.board.enpassantTarget + forwardOffset
         if ((pawns shr leftPawn) and 1UL == 1UL) {
             moves.add(Move(leftPawn, to, type = Move.EP_CAPTURE))
         }
@@ -241,19 +233,17 @@ private fun generatePawnMoves(
 }
 
 private fun generateKnightMoves(
-    board: Board,
     moves: MutableList<Move>,
+    data: MoveGenData,
 ) {
-    var knights = board.getKnightBB()
-    val nonblockers = board.getColorBB().inv()
-    val opp = board.getOpponentBB()
+    var knights = data.board.getKnightBB()
     if (knights != 0UL) {
         var (lsb1, from) = lsb(knights)
         while (lsb1 != 0UL) {
-            var attacks = knightAttacks[from]!! and nonblockers
+            var attacks = knightAttacks[from]!! and data.emptyOrOppSqrs
             var (lsb2, to) = lsb(attacks)
             while (lsb2 != 0UL) {
-                val type = if (lsb2 and opp != 0UL) Move.CAPTURE else Move.QUIET
+                val type = if (lsb2 and data.oppPieces != 0UL) Move.CAPTURE else Move.QUIET
                 moves.add(Move(from, to, type))
 
                 attacks = attacks xor lsb2
@@ -273,20 +263,18 @@ private fun generateKnightMoves(
 }
 
 private fun generateKingMoves(
-    board: Board,
     moves: MutableList<Move>,
+    data: MoveGenData,
 ) {
-    val king = board.getKingBB()
-    val blockers = board.getColorBB()
-    val opp = board.getOpponentBB()
+    val king = data.board.getKingBB()
     val (_, from) = lsb(king)
 
 //    Normal move
-    var attacks = kingAttacks[from]!! and blockers.inv()
+    var attacks = kingAttacks[from]!! and data.emptySqrs
     if (attacks != 0UL) {
         var (lsb, to) = lsb(attacks)
         while (lsb != 0UL) {
-            val type = if (lsb and opp != 0UL) Move.CAPTURE else Move.QUIET
+            val type = if (lsb and data.oppPieces != 0UL) Move.CAPTURE else Move.QUIET
             moves.add(Move(from, to, type))
 
             attacks = attacks xor lsb
@@ -298,33 +286,58 @@ private fun generateKingMoves(
     }
 
 //    Castling
-    if (board.castlingRights != 0.b) {
-        val isWhite = board.side == Piece.WHITE
+    if (data.board.castlingRights != 0.b) {
+        val isWhite = data.board.side == Piece.WHITE
         val backrank = if (isWhite) RANK_1 else RANK_8
         val sideMask: Byte = if (isWhite) 0b1100 else 0b0011
         if (
-            board.castlingRights and sideMask and 0b0101 != 0.b &&
-            backrank and K_CASTLE_CHECK and blockers == 0UL
+            data.board.castlingRights and sideMask and 0b0101 != 0.b &&
+            backrank and K_CASTLE_CHECK and data.ownPieces == 0UL
         ) {
             moves.add(Move(from, to = from + 2, type = Move.K_CASTLE))
         }
         if (
-            board.castlingRights and sideMask and 0b1010 != 0.b &&
-            backrank and Q_CASTLE_CHECK and blockers == 0UL
+            data.board.castlingRights and sideMask and 0b1010 != 0.b &&
+            backrank and Q_CASTLE_CHECK and data.ownPieces == 0UL
         ) {
             moves.add(Move(from, to = from - 2, type = Move.Q_CASTLE))
         }
     }
 }
 
+data class MoveGenData(
+    val board: Board,
+) {
+    var inCheck = false
+    var inDoubleCheck = false
+    var checkRayBitmask = 0UL
+    var pinRays = 0UL
+
+    // Store some info for convenience
+    val isWhite = board.side == Piece.WHITE
+    val ownClr = board.side
+    val oppClr = if (isWhite) Piece.BLACK else Piece.WHITE
+    val ownKingSqr = lsb(board.getKingBB()).second
+
+    // Store some bitboards for convenience
+    val oppPieces = board.getOpponentBB()
+    val ownPieces = board.getColorBB()
+    val allPieces = board.occupancyBB
+    val emptySqrs = allPieces.inv()
+    val emptyOrOppSqrs = emptySqrs or oppPieces
+}
+
 object MoveGen {
-    fun pseudoLegal(board: Board): List<Move> {
+    fun generateMoves(board: Board): List<Move> {
         val moves = mutableListOf<Move>()
-        generateKingMoves(board, moves)
-        generateOrthogonalMoves(board, moves)
-        generateDiagonalMoves(board, moves)
-        generatePawnMoves(board, moves)
-        generateKnightMoves(board, moves)
+        val moveGenData = MoveGenData(board)
+
+        generateKingMoves(moves, moveGenData)
+        generateOrthogonalMoves(moves, moveGenData)
+        generateDiagonalMoves(moves, moveGenData)
+        generatePawnMoves(moves, moveGenData)
+        generateKnightMoves(moves, moveGenData)
+
         return moves
     }
 }
