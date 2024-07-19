948ce377787fdc7eb88bf1904e37f03483f0daea
Clikuki <lanzvasquez24@gmail.com>
2024-07-19 23:17:38 +0800

    fixed incorrect setting of eptarget in Board.from()

--- src/main/kotlin/Board.kt
+++ src/main/kotlin/Board.kt
@@ -348,12 +348,12 @@ class Board {
                     when (it[0]) {
                         'a' -> 0
                         'b' -> 1
-                        'c' -> 3
-                        'd' -> 4
-                        'e' -> 5
-                        'f' -> 6
-                        'g' -> 7
-                        'h' -> 8
+                        'c' -> 2
+                        'd' -> 3
+                        'e' -> 4
+                        'f' -> 5
+                        'g' -> 6
+                        'h' -> 7
                         else -> throw Error("INVALID FEN - EP SQR")
                     }
 
