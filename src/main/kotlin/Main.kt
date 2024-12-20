aadbef8301273ed9735bd2ae92b72ebde7da010c
Lanz Vasquez <LanzVasquez24@gmail.com>
2024-12-20 23:12:55 +0800

    can now select which piece to promote to

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,9 +2,11 @@ import org.openrndr.application
 import org.openrndr.color.ColorRGBa
 import org.openrndr.draw.isolated
 import org.openrndr.draw.loadImage
+import org.openrndr.extra.color.presets.LIME_GREEN
 import org.openrndr.math.Matrix55
 import org.openrndr.math.Vector2
 import org.openrndr.shape.Rectangle
+import kotlin.experimental.and
 import kotlin.experimental.or
 import kotlin.time.measureTime
 
@@ -51,6 +53,7 @@ fun main() =
         }
         program {
             val board = Board.from("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+//            val board = Board.from("8/PPPPPPPP/8/8/8/8/R7/K6k w - - 0 1")
             val moveStack = ArrayDeque<Move>()
             val moveGen = MoveGen(board)
             moveGen.generateMoves()
@@ -72,8 +75,12 @@ fun main() =
 
             val tileSize = height / 9.0
             val edgeOffset = (height - tileSize * 8) / 2
+            val promoOptions = listOf(Piece.QUEEN, Piece.ROOK, Piece.BISHOP, Piece.KNIGHT)
+            val promoOptionsX = width - 50 - tileSize * 9
+
             var mousePos: Vector2? = null
             var pieceMoveFromSqr: Int? = null
+            var promoteToPc: Byte = Piece.QUEEN
 
 //            Helper functions
             fun isWithinBoard(vec: Vector2) = vec.x >= 0 && vec.x < tileSize * 8 && vec.y >= 0 && vec.y < tileSize * 8
@@ -83,6 +90,20 @@ fun main() =
                     vec.x - (width - (8 * tileSize) - edgeOffset),
                     vec.y - edgeOffset,
                 )
+
+            fun correctMove(mv: Move, toSqr: Int): Boolean {
+                if(mv.from != pieceMoveFromSqr) return false
+                if(mv.to != toSqr) return false
+
+                if(mv.type and 0b1000.b == 0.b) return true
+                return mv.type and 0b0011 == 0b0011.b and when(promoteToPc) {
+                    Piece.KNIGHT -> Move.N_PROMO_CAPTURE
+                    Piece.BISHOP -> Move.B_PROMO_CAPTURE
+                    Piece.ROOK -> Move.R_PROMO_CAPTURE
+                    Piece.QUEEN -> Move.Q_PROMO_CAPTURE
+                    else -> throw Exception("Invalid variable: promoteToPc")
+                }
+            }
 
             mouse.moved.listen { mousePos = it.position }
             mouse.buttonDown.listen { e ->
@@ -95,11 +116,12 @@ fun main() =
             mouse.buttonUp.listen { e ->
                 val offsetPos = getFixedBoardVec(e.position)
 
+//                Piece drop-off
                 if (pieceMoveFromSqr != null && isWithinBoard(offsetPos)) {
                     val toSqr = (offsetPos / tileSize).toInt().let { it.y * 8 + it.x }
 
                     mvLoop@for (move in moveGen.moves) {
-                        if (move.from != pieceMoveFromSqr || move.to != toSqr) continue
+                        if (!correctMove(move, toSqr)) continue
 //                        println(move)
 
 //                        Player
@@ -122,6 +144,22 @@ fun main() =
                         moveGen.generateMoves()
 
                         break@mvLoop
+                    }
+                }
+
+//                Pawn promotion options
+                if(
+                    e.position.x >= promoOptionsX &&
+                    e.position.x <= promoOptionsX + tileSize &&
+                    e.position.y >= edgeOffset &&
+                    e.position.y <= promoOptions.size * tileSize + edgeOffset
+                ) {
+                    for((i, pc) in promoOptions.withIndex()) {
+                        val bottomEdge = tileSize * (i+1) + edgeOffset
+                        if(e.position.y <= bottomEdge) {
+                            promoteToPc = pc
+                            break
+                        }
                     }
                 }
 
@@ -148,17 +186,43 @@ fun main() =
             extend {
                 drawer.clear(windowBG)
 
+//                Pawn promotion selection
+                promoOptions.forEachIndexed { i, pc ->
+                    if(promoteToPc == pc) {
+                        drawer.fill = ColorRGBa.LIME_GREEN
+                        drawer.stroke = ColorRGBa.BLACK
+                        drawer.strokeWeight = 2.0
+                    } else {
+                        drawer.fill = ColorRGBa.fromHex("0x00000022")
+                        drawer.stroke = null
+                        drawer.strokeWeight = 0.0
+                    }
+
+                    val screenY = tileSize * i + edgeOffset
+                    if(promoteToPc == pc) drawer.rectangle(promoOptionsX, screenY, tileSize)
+                    drawer.image(
+                        pieceSpriteSheet,
+                        pieceLoc[pc or Piece.WHITE]!!,
+                        Rectangle(promoOptionsX, screenY, tileSize),
+                    )
+                    if(promoteToPc != pc) drawer.rectangle(promoOptionsX, screenY, tileSize)
+                }
+                drawer.fill = null
+                drawer.stroke = ColorRGBa.BLACK
+                drawer.strokeWeight = 2.0
+                drawer.rectangle(promoOptionsX, edgeOffset, tileSize, promoOptions.size * tileSize)
+
 //                Loop through each board tile
                 drawer.stroke = null
                 for (x in 0..7) {
                     for (y in 0..7) {
                         val index = y * 8 + x
                         val piece = board.get(index)
-                        val screenx = width - ((8 - x) * tileSize) - edgeOffset
-                        val screeny = y * tileSize + edgeOffset
+                        val screenX = width - ((8 - x) * tileSize) - edgeOffset
+                        val screenY = y * tileSize + edgeOffset
 
                         drawer.fill = if ((x + y) % 2 == 0) lightTile else darkTile
-                        drawer.rectangle(screenx, screeny, tileSize)
+                        drawer.rectangle(screenX, screenY, tileSize)
 
 // //                        Draw bit clr from bitboard
 //                        if (options.displayBitboards) {
@@ -166,7 +230,7 @@ fun main() =
 //                            for ((bb, clr) in bitboardTrackers) {
 //                                if (bb() and mask != 0UL) {
 //                                    drawer.fill = clr
-//                                    drawer.rectangle(screenx, screeny, tileSize)
+//                                    drawer.rectangle(screenX, screenY, tileSize)
 //                                }
 //                            }
 //                        }
@@ -174,7 +238,7 @@ fun main() =
 //                        Highlight checked kings
                         if (moveGen.data.inCheck && piece == board.side or Piece.KING) {
                             drawer.fill = checkHighlight
-                            drawer.rectangle(screenx, screeny, tileSize)
+                            drawer.rectangle(screenX, screenY, tileSize)
                         }
 
 //                        Highlight move start/end tile
@@ -183,7 +247,7 @@ fun main() =
                             (pieceMoveFromSqr!! == index || moveGen.moves.any { it.from == pieceMoveFromSqr!! && it.to == index })
                         ) {
                             drawer.fill = tileHighlight
-                            drawer.rectangle(screenx, screeny, tileSize)
+                            drawer.rectangle(screenX, screenY, tileSize)
                         }
                         if (piece == Piece.NONE) continue
 
@@ -196,7 +260,7 @@ fun main() =
                             drawer.image(
                                 pieceSpriteSheet,
                                 pieceLoc[piece]!!,
-                                Rectangle(screenx, screeny, tileSize),
+                                Rectangle(screenX, screenY, tileSize),
                             )
                         }
                     }
